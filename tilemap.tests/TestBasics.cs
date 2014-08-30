341081906e2eabb6beb7d3da2b80a96cffb0f395
Mattias Wargren <mattias.wargren@gmail.com>
2014-08-30 23:57:44 +0200

    Initial commit

---
+++ tilemap.tests/TestBasics.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) 2014 Mattias Wargren
+using NUnit.Framework;
+
+namespace tilemap.tests
+{
+	[TestFixture()]
+	public class TestBasics
+	{
+		[Test()]
+		public void TestTileMapCreation()
+		{
+			var tileMap = new TileMap<int>(10, 10);
+			Assert.IsNotNull(tileMap);
+		}
+
+		[Test()]
+		public void TestTileMapCreationDefaultList()
+		{
+			var tileMap = new TileMap<int>(3, 1, new int[]{ 1, 2, 3 });
+			Assert.AreEqual(3, tileMap.Count);
+		}
+
+		[Test()]
+		public void TestTileMapSize()
+		{
+			var tileMap = new TileMap<int>(3, 10);
+
+			Assert.AreEqual(3, tileMap.width);
+			Assert.AreEqual(10, tileMap.height);
+			Assert.AreEqual(30, tileMap.Count);
+		}
+
+		[Test()]
+		public void TestTileMapGetTileIndex()
+		{
+			var tileMap = new TileMap<string>(3, 3);
+			var tileIndex = 0;
+
+			for (var y = 0; y < tileMap.height; y++)
+			{
+				for (var x = 0; x < tileMap.width; x++)
+				{
+					Assert.AreEqual(tileIndex, tileMap.GetTileIndex(x, y));
+					tileIndex++;
+				}
+			}
+		}
+
+		[Test()]
+		public void TestTileMapSetAndGetByCoordinates()
+		{
+			var tileMap = new TileMap<string>(3, 1);
+
+			tileMap.SetTileByCoordinates(0, 0, "0-0");
+			tileMap.SetTileByCoordinates(1, 0, "1-0");
+			tileMap.SetTileByCoordinates(2, 0, "2-0");
+
+			Assert.AreEqual("0-0", tileMap.GetTileByCoordinates(0, 0));
+			Assert.AreEqual("1-0", tileMap.GetTileByCoordinates(1, 0));
+			Assert.AreEqual("2-0", tileMap.GetTileByCoordinates(2, 0));
+		}
+
+		[Test()]
+		public void TestTileMapSetAndGetByIndex()
+		{
+			var tileMap = new TileMap<int>(10, 3);
+
+			for (var i = 0; i < tileMap.Count; i++)
+			{
+				tileMap.SetTileByIndex(i, i);
+			}
+
+			for (var i = 0; i < tileMap.Count; i++)
+			{
+				Assert.AreEqual(i, tileMap.GetTileByIndex(i));
+			}
+		}
+
+		[Test()]
+		public void TestTileMapFillByIndex()
+		{
+			var tileMap = new TileMap<string>(3, 3);
+			tileMap.Fill((pIndex) => string.Format("index-{0}", pIndex));
+
+			for (var i = 0; i < tileMap.Count; i++)
+			{
+				Assert.AreEqual(string.Format("index-{0}", i), tileMap.GetTileByIndex(i));
+			}
+		}
+
+		[Test()]
+		public void TestTileMapFillByCoordinates()
+		{
+			var tileMap = new TileMap<string>(3, 3);
+			tileMap.Fill((pX, pY) => string.Format("x{0} y{1}", pX, pY));
+		}
+
+		[Test()]
+		public void TestTileMapNeighbours()
+		{
+			var tileMap = new TileMap<string>(3, 3);
+
+			tileMap.Fill((pIndex) => string.Format("index-{0}", pIndex));
+
+			var tileNeighboursTL = tileMap.GetTileNeighboursByCoordinates(0, 0);
+			var tileNeighboursML = tileMap.GetTileNeighboursByCoordinates(0, 1);
+			var tileNeighboursMC = tileMap.GetTileNeighboursByCoordinates(1, 1);
+			var tileNeighboursMR = tileMap.GetTileNeighboursByCoordinates(2, 1);
+			var tileNeighboursBR = tileMap.GetTileNeighboursByCoordinates(2, 2);
+
+			Assert.AreEqual(3, tileNeighboursTL.Count);
+			Assert.AreEqual(5, tileNeighboursML.Count);
+			Assert.AreEqual(8, tileNeighboursMC.Count);
+			Assert.AreEqual(5, tileNeighboursMR.Count);
+			Assert.AreEqual(3, tileNeighboursBR.Count);
+
+			Assert.AreEqual("index-1", tileNeighboursTL[0]);
+			Assert.AreEqual("index-3", tileNeighboursTL[1]);
+			Assert.AreEqual("index-4", tileNeighboursTL[2]);
+		}
+
+	}
+}
+
---
+++ tilemap/TileMap.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) 2014 Mattias Wargren
+using System;
+using System.Collections.Generic;
+
+namespace tilemap
+{
+	public class TileMap<T>
+	{
+		T[] _tiles;
+		int _width;
+		int _height;
+
+		public TileMap(int pWidth, int pHeight, T[] pTiles = null)
+		{
+			_width = pWidth;
+			_height = pHeight;
+
+			if (pTiles == null)
+			{
+				_tiles = new T[_width * _height];
+			}
+			else
+			{
+				if (pTiles.Length == (_width * _height))
+				{
+					_tiles = pTiles;
+				}
+				else
+				{
+					throw new Exception(string.Format("pTiles should have {0} items, found {1}", (_width * _height), pTiles.Length));
+				}
+			}
+		}
+
+		public int Count {
+			get {
+				return _tiles.Length;
+			}
+		}
+
+		public int width {
+			get {
+				return _width;
+			}
+		}
+
+		public int height {
+			get {
+				return _height;
+			}
+		}
+
+		public int GetTileIndex(int pX, int pY)
+		{
+			return (pY * _width) + pX;
+		}
+
+		bool ValidCoordinates(int pX, int pY)
+		{
+			return (pX >= 0 && pX < _width && pY >= 0 && pY < _height) ? true : false;
+		}
+
+		public T GetTileByCoordinates(int pX, int pY)
+		{
+			if (ValidCoordinates(pX, pY))
+			{
+				var index = GetTileIndex(pX, pY);
+				return _tiles[index];
+			}
+			else
+			{
+				return default(T);
+			}
+		}
+
+		public T SetTileByCoordinates(int pX, int pY, T pTile)
+		{
+			var tileIndex = GetTileIndex(pX, pY);
+			return _tiles[tileIndex] = pTile;
+		}
+
+		public void SetTileByIndex(int pIndex, T pTile)
+		{
+			_tiles[pIndex] = pTile;
+		}
+
+		public T GetTileByIndex(int pIndex)
+		{
+			return _tiles[pIndex];
+		}
+
+		public void Fill(Func<int, T> pFunc)
+		{
+			for (var i = 0; i < _tiles.Length; i++)
+			{
+				SetTileByIndex(i, pFunc.Invoke(i));
+			}
+		}
+
+		public void Fill(Func<int, int, T> pFunc)
+		{
+			for (var y = 0; y < _height; y++)
+			{
+				for (var x = 0; x < _width; x++)
+				{
+					SetTileByCoordinates(x, y, pFunc.Invoke(x, y));
+				}
+			}
+		}
+
+		public List<T> GetTileNeighboursByCoordinates(int pX, int pY)
+		{
+			var result = new List<T>();
+
+			for (var y = 0; y < _height; y++)
+			{
+				for (var x = 0; x < _width; x++)
+				{
+					if (!(x == pX && y == pY) && Math.Abs(pX - x) <= 1 && Math.Abs(pY - y) <= 1)
+					{
+						result.Add(GetTileByCoordinates(x, y));
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
